32978319b6f95e8e906b68f6ff7febceb06e88c3
Bhavyatandon <tandonbhavya03@gmail.com>
2025-10-13 15:16:00 +0530

    Initial commit of deafult imdb template, Part 01 of mini_app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+
+import Navbar from './Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold bg-sky-100'>Hello</h1>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<div>Home page loaded</div>} />
+      <Route path='/watchlist' element={<div>Watchlist page loaded</div>} />
+      
+    </Routes>
+    </>
+  )
+}
+
+export default App
